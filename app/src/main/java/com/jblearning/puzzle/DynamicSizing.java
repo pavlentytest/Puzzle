ee9dea8bed4c5dd78a93e750c7615e79bd513357
Pvl <impetus@yandex.ru>
2019-10-05 19:28:49 +0300

    Initial commit

---
+++ app/src/main/java/com/jblearning/puzzle/DynamicSizing.java
@@ -0,0 +1,28 @@
+package com.jblearning.puzzle;
+
+import android.util.TypedValue;
+import android.view.View.MeasureSpec;
+import android.widget.TextView;
+
+public class DynamicSizing {
+  public static final int MAX_FONT_SIZE = 200;
+  public static final int MIN_FONT_SIZE = 1;
+
+
+  public static int setFontSizeToFitInView( TextView tv ) {
+    int fontSize = MAX_FONT_SIZE;
+    tv.setTextSize( TypedValue.COMPLEX_UNIT_SP, fontSize );
+    tv.measure( MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED );
+    int lines = tv.getLineCount( );
+    if( lines > 0 ) {
+      while( lines != 1 && fontSize >= MIN_FONT_SIZE + 2 ) {
+        fontSize--;
+        tv.setTextSize( TypedValue.COMPLEX_UNIT_SP, fontSize );
+        tv.measure( MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED );
+        lines = tv.getLineCount( );
+      }
+      tv.setTextSize( TypedValue.COMPLEX_UNIT_SP, --fontSize );
+    }
+    return fontSize;
+  }
+}
